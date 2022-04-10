39473d5c0c07b674c3e4ff5dc1cc45b79cb1914f
mgarciate <mgarciate@gmail.com>
2022-04-10 23:24:54 +0200

    Add Hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config({path: './process.env'})
+
+const { GOERLI_URL, RINKEBY_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: `${GOERLI_URL}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `${RINKEBY_URL}`,
+      accounts: [`${PRIVATE_KEY}`],
+    }
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`,
+  },
+  solidity: "0.8.4",
+};
